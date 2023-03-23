403b1691d81385635cc33291235af2bd2007f9dc
Marina Krause Eskelsen <marina.eskelsen@acad.ufsm.br>
2023-03-23 14:37:29 -0300

    Enunciado

--- Calculo_Valor_Produto/main.py
+++ Calculo_Valor_Produto/main.py
@@ -1,3 +1,10 @@
+'''Desenvolva um algoritmo que leia o valor a ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento. Para realizar o cálculo, utilize os códigos a seguir referentes à condição de pagamento escolhida pelo cliente.
+-Código Condição de pagamento;
+-À vista em dinheiro ou cheque, recebe 10% de desconto.
+-À vista no cartão de crédito, recebe 15% de desconto.
+-Em duas vezes, preço normal de etiqueta mais juros de 10%.
+'''
+
 def calcula_preco(valor, cod):
     if cod == 1: #A.V dinheiro ou cheque
         valor = valor*0.9
