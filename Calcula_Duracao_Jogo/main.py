67b6591c029c935ed35c654efefdf1e05fd947c6
Marina Krause Eskelsen <marina.eskelsen@acad.ufsm.br>
2023-03-23 18:32:50 -0300

    Calculo de horas funcionando

--- Calcula_Duracao_Jogo/main.py
+++ Calcula_Duracao_Jogo/main.py
@@ -3,15 +3,22 @@ ambas subdivididas em 2 valores distintos: horas e minutos.
 A função deve apresentar a duração do jogo em horas e minutos,
 considerando que o tempo máximo de duração de um jogo é de 24 horas e que o jogo pode começar em um dia e terminar no outro.'''
 
+from math import trunc
 
 def calcula_duracao_jogo(hInicio, minInicio, hFim, minFim):
     hInicio = (hInicio * 60) + minInicio #Converter para mins
     hFim = (hFim * 60) + minFim
-    duracao = hFim - hInicio
-    hDuracao = duracao / 60
+
+    if hFim>hInicio:
+        duracao = hFim - hInicio
+    else:
+        #Caso a hora de inicio seja maior que a hora de fim, então começa em um dia e termine no outro
+        duracao = 1440 - (hInicio-hFim) #1440 eh o total de minutos do dia
+
+    hDuracao = trunc(duracao / 60)
     minDuracao = duracao % 60
 
-    print("A duração do jogo é de %d horas e %d horas" (hDuracao, minDuracao))
+    print(f"A duração do jogo é de {hDuracao} horas e {minDuracao} minutos")
 
 
 if __name__ == '__main__':
